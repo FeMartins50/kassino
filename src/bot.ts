090fa3ed73203897bce7ec03e1f628c40254bed4
FeMartins50 <nando.souto@hotmail.com>
2022-01-01 15:12:02 -0300

    Uns trem aí

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,48 @@
-console.log("vai tomar no uepa kkkkkk rapaaaaz");
+( await import("dotenv") ).config();
+const token: string = process.env.TOKEN as string;
+
+import { readFileSync } from "fs";
+import { Client, Intents } from "discord.js";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+const rawConfig = readFileSync("./config.json").toString();
+const config = JSON.parse(rawConfig);
+console.log(config);
+
+const bot: Client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ] 
+});
+
+
+
+
+bot.on("interactionCreate", interaction => {
+  if(!interaction.isCommand()){
+    return;
+  }
+  console.log(interaction);
+  interaction.reply("blz mano");
+});
+
+try{
+  await bot.login(token);
+} catch(err){
+  console.error(err);
+}
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+try{
+  rest.put(Routes.applicationGuildCommands(bot.user!.id, config.guildIds.teste), { body: commands });
+} catch (err) {
+  console.error(err);
+}
